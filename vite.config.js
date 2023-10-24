5bbf591a7fbd84bfc07da79d7e22027316dcbf26
George Strakhov <george.strakhov@gmail.com>
2023-10-24 17:33:29 +0200

    switched to docs for silly github

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
-  server: {
-    host: '0.0.0.0',
-  }
+    plugins: [],
+    server: {
+        host: '0.0.0.0',
+    },
+    build: {
+        outDir: 'docs'
+    }
 })
